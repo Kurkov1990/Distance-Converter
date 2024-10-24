3b49c2744681779ee997024d33f2fccf7011dc8c
Andrii Kurkov <kurkovandrey90@gmail.com>
2024-10-24 22:15:36 +0300

    Add convertation miles to kms

--- src/app/Main.java
+++ src/app/Main.java
@@ -6,5 +6,14 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("App for distance converting.");
+
+        double mls = 25;
+        double kilomtrs = convMlsToKms(mls);
+        System.out.println("Result is " + kilomtrs + " kilometers");
     }
+
+    private static double convMlsToKms(double mls) {
+        return mls/CONV_K;
+    }
+
 }

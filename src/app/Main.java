d73ffa5732091fac065fb1b3efdb10a238ccd4be
Andrii Kurkov <kurkovandrey90@gmail.com>
2024-10-24 22:06:39 +0300

    Initial commit

---
+++ src/app/Main.java
@@ -0,0 +1,10 @@
+package app;
+
+public class Main {
+
+    private static double CONV_K = 0.62137;
+
+    public static void main(String[] args) {
+        System.out.println("App for distance converting.");
+    }
+}
